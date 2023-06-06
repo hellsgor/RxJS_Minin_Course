36d29e5cd2a88bb0e46d4c33a96163e458808436
hellsgor <hellsgor@gmail.com>
2023-06-07 01:26:22 +0400

    studied Subjects
    - Subject
    - BehaviorSubject
    - ReplaySubject
    - add processing css-files to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
-const path = require('path')
-const HTMLPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
   },
   devServer: {
-    port: 4200
+    port: 4200,
   },
   plugins: [
     new HTMLPlugin({
-      template: './src/index.html'
-    })
-  ]
-}
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
